68ec2b0fb88e4d73f5f417763aac7706831f4736
Your Name <decierdo.dexter@dnsc.edu.ph>
2022-11-16 22:51:22 +0800

    Initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'load',
+    pathMatch: 'full'
+  },
+  {
+    path: 'load',
+    loadChildren: () => import('./pages/load/load.module').then( m => m.LoadPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
